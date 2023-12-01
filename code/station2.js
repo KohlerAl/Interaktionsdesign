586966f2cf49aec5352f8c381475c7ba3460876a
KohlerAl <56314717+KohlerAl@users.noreply.github.com>
2023-12-01 11:49:57 +0100

    safe

--- code/station2.js
+++ code/station2.js
@@ -37,18 +37,27 @@ var Station2_1;
         canvas.height = 500;
         /* crc.fillStyle = "white";
         crc.fillRect(0, 0, 370, 185); */
-        crc.drawImage(mapImage, 0, 0, 740, 500);
+        crc.drawImage(mapImage, -350, 0, 740 * 1.5, 500 * 1.5);
+        crc.drawImage(pink, -350, 0, 740 * 1.5, 500 * 1.5);
+        crc.drawImage(beige, -350, 0, 740 * 1.5, 500 * 1.5);
+        crc.drawImage(green, -350, 0, 740 * 1.5, 500 * 1.5);
+        crc.drawImage(brown, -350, 0, 740 * 1.5, 500 * 1.5);
+        /*
+        crc.drawImage(mapImage, -0, 0, 740, 500); */
         localStorage.setItem("station2", "0");
         localStorage.setItem("points", "0");
     }
     function handleDown(_event) {
         let x = _event.clientX;
-        let y = _event.clientY;
-        console.log(tries);
+        let y = _event.clientY - canvas.getBoundingClientRect().y;
+        /* let y: number = _event.clientY;  */
+        console.log("X: " + x, "  Y: " + y);
         if (tries < 12) {
-            if (checkInside(x, y, 165, 315, 205, 350)) {
+            /* if (checkInside(x, y, 165, 315, 205, 350)) */
+            if (checkInside(x, y, 65, 95, 135, 155)) {
+                console.log("beige clicky");
                 if (!be) {
-                    crc.drawImage(beige, 0, 0, 740, 500);
+                    crc.drawImage(beige, -350, 0, 740 * 1.5, 500 * 1.5);
                     be = true;
                     btn.style.display = "block";
                     btn.innerHTML = "Fast geschafft!";
@@ -56,9 +65,11 @@ var Station2_1;
                         btn.classList.add("florian");
                 }
             }
-            else if (checkInside(x, y, 205, 310, 250, 340)) {
+            /* else if (checkInside(x, y, 205, 310, 250, 340)) */
+            else if (checkInside(x, y, 135, 110, 200, 140)) {
+                console.log("green clicky");
                 if (!gr) {
-                    crc.drawImage(green, 0, 0, 740, 500);
+                    crc.drawImage(green, -350, 0, 740 * 1.5, 500 * 1.5);
                     gr = true;
                     btn.style.display = "block";
                     btn.innerHTML = "Fast geschafft!";
@@ -66,9 +77,11 @@ var Station2_1;
                         btn.classList.add("florian");
                 }
             }
-            else if (checkInside(x, y, 250, 310, 310, 330)) {
+            /* else if (checkInside(x, y, 250, 310, 310, 330))  */
+            else if (checkInside(x, y, 200, 100, 345, 135)) {
+                console.log("brown clicky");
                 if (!br) {
-                    crc.drawImage(brown, 0, 0, 740, 500);
+                    crc.drawImage(brown, -350, 0, 740 * 1.5, 500 * 1.5);
                     br = true;
                     btn.style.display = "block";
                     btn.innerHTML = "Fast geschafft!";
@@ -76,9 +89,11 @@ var Station2_1;
                         btn.classList.add("florian");
                 }
             }
-            else if (checkInside(x, y, 255, 330, 345, 355)) {
+            /* else if (checkInside(x, y, 255, 330, 345, 355)) */
+            else if (checkInside(x, y, 200, 130, 290, 165)) {
+                console.log("pink clicky");
                 if (!pi) {
-                    crc.drawImage(pink, 0, 0, 740, 500);
+                    crc.drawImage(pink, -350, 0, 740 * 1.5, 500 * 1.5);
                     pi = true;
                     btn.style.display = "block";
                     btn.innerHTML = "Fast geschafft!";
@@ -86,7 +101,7 @@ var Station2_1;
                         btn.classList.add("florian");
                 }
             }
-            crc.drawImage(mapImage, 0, 0, 740, 500);
+            crc.drawImage(mapImage, -350, 0, 740 * 1.5, 500 * 1.5);
             if (pi && gr && br && be) {
                 next.style.display = "block";
                 btn.classList.add("right");
--- code/station2.ts
+++ code/station2.ts
@@ -48,20 +48,29 @@ namespace Station2_1 {
         canvas.height = 500;
         /* crc.fillStyle = "white"; 
         crc.fillRect(0, 0, 370, 185); */
-        crc.drawImage(mapImage, 0, 0, 740, 500);
+        crc.drawImage(mapImage, -350, 0, 740 * 1.5 , 500 * 1.5);
+        crc.drawImage(pink, -350, 0, 740 * 1.5 , 500 * 1.5);
+        crc.drawImage(beige, -350, 0, 740 * 1.5 , 500 * 1.5);
+        crc.drawImage(green, -350, 0, 740 * 1.5 , 500 * 1.5);
+        crc.drawImage(brown, -350, 0, 740 * 1.5 , 500 * 1.5);
+        /* 
+        crc.drawImage(mapImage, -0, 0, 740, 500); */
 
-        localStorage.setItem("station2", "0"); 
-        localStorage.setItem("points", "0"); 
+        localStorage.setItem("station2", "0");
+        localStorage.setItem("points", "0");
     }
 
     function handleDown(_event: PointerEvent): void {
         let x: number = _event.clientX;
-        let y: number = _event.clientY;
-        console.log(tries)
+        let y: number = _event.clientY - canvas.getBoundingClientRect().y;
+        /* let y: number = _event.clientY;  */
+        console.log("X: " + x, "  Y: " + y); 
         if (tries < 12) {
-            if (checkInside(x, y, 165, 315, 205, 350)) {
+            /* if (checkInside(x, y, 165, 315, 205, 350)) */ 
+            if (checkInside(x, y, 65, 95, 135, 155)){
+                console.log("beige clicky"); 
                 if (!be) {
-                    crc.drawImage(beige, 0, 0, 740, 500);
+                    crc.drawImage(beige,-350, 0, 740 * 1.5 , 500 * 1.5);
                     be = true;
                     btn.style.display = "block";
                     btn.innerHTML = "Fast geschafft!";
@@ -69,9 +78,11 @@ namespace Station2_1 {
                         btn.classList.add("florian");
                 }
             }
-            else if (checkInside(x, y, 205, 310, 250, 340)) {
+            /* else if (checkInside(x, y, 205, 310, 250, 340)) */ 
+            else if (checkInside(x, y, 135, 110, 200, 140)){
+                console.log("green clicky")
                 if (!gr) {
-                    crc.drawImage(green, 0, 0, 740, 500);
+                    crc.drawImage(green,-350, 0, 740 * 1.5 , 500 * 1.5);
                     gr = true;
                     btn.style.display = "block";
                     btn.innerHTML = "Fast geschafft!";
@@ -79,9 +90,11 @@ namespace Station2_1 {
                         btn.classList.add("florian");
                 }
             }
-            else if (checkInside(x, y, 250, 310, 310, 330)) {
+            /* else if (checkInside(x, y, 250, 310, 310, 330))  */
+            else if (checkInside(x, y, 200, 100, 345, 135)) {
+                console.log("brown clicky")
                 if (!br) {
-                    crc.drawImage(brown, 0, 0, 740, 500);
+                    crc.drawImage(brown, -350, 0, 740 * 1.5 , 500 * 1.5);
                     br = true;
                     btn.style.display = "block";
                     btn.innerHTML = "Fast geschafft!";
@@ -89,9 +102,11 @@ namespace Station2_1 {
                         btn.classList.add("florian");
                 }
             }
-            else if (checkInside(x, y, 255, 330, 345, 355)) {
+            /* else if (checkInside(x, y, 255, 330, 345, 355)) */
+            else if (checkInside(x, y, 200, 130, 290, 165)) {
+                console.log("pink clicky")
                 if (!pi) {
-                    crc.drawImage(pink, 0, 0, 740, 500);
+                    crc.drawImage(pink, -350, 0, 740 * 1.5 , 500 * 1.5);
                     pi = true;
                     btn.style.display = "block";
                     btn.innerHTML = "Fast geschafft!";
@@ -99,7 +114,7 @@ namespace Station2_1 {
                         btn.classList.add("florian");
                 }
             }
-            crc.drawImage(mapImage, 0, 0, 740, 500);
+            crc.drawImage(mapImage, -350, 0, 740 * 1.5 , 500 * 1.5);
             if (pi && gr && br && be) {
                 next.style.display = "block";
                 btn.classList.add("right");
@@ -110,12 +125,12 @@ namespace Station2_1 {
                 let points: number = Number(localStorage.getItem("points"));
                 points += 10;
                 localStorage.setItem("points", points + "");
-                canvas.removeEventListener("pointerdown", handleDown); 
+                canvas.removeEventListener("pointerdown", handleDown);
 
-                localStorage.setItem("station2", "10"); 
-                final(); 
+                localStorage.setItem("station2", "10");
+                final();
             }
-           
+
         }
         else {
             btn.style.display = "block";
@@ -124,9 +139,9 @@ namespace Station2_1 {
             if (btn.classList.contains("florian"))
                 btn.classList.add("florian");
 
-            canvas.removeEventListener("pointerdown", handleDown); 
-            localStorage.setItem("station2", "2"); 
-            final(); 
+            canvas.removeEventListener("pointerdown", handleDown);
+            localStorage.setItem("station2", "2");
+            final();
         }
 
         tries++;
@@ -144,9 +159,9 @@ namespace Station2_1 {
     }
 
     function final(): void {
-        let station1: number = Number(localStorage.getItem("station1")); 
-        let station2: number = Number(localStorage.getItem("station2")); 
-        localStorage.setItem("points", station1 + station2 + ""); 
+        let station1: number = Number(localStorage.getItem("station1"));
+        let station2: number = Number(localStorage.getItem("station2"));
+        localStorage.setItem("points", station1 + station2 + "");
         console.log(localStorage.getItem("points"))
     }
 }
